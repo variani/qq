39a047b7ba5659b95965d506593b456e17fc24f0
variani <machovariani@gmail.com>
2017-04-04 15:52:21 -0400

    qq_plot

---
+++ R/package.R
@@ -0,0 +1,10 @@
+#' Package: qq
+#'
+#' @name copdgeneQQ
+#' @docType package
+#'
+#' @import dplyr
+#' @import ggplot2
+#'
+NULL
+
---
+++ R/plots.R
@@ -0,0 +1,58 @@
+
+#' @export
+qq_inflation <- function(pvals, thr_pval, ...)
+{
+  #if(!missing(thr_pval)) {
+  #  pvals <- pvals[pvals > thr_pval]
+  #}
+  
+  median(qchisq(pvals, 1, lower.tail = FALSE)) / qchisq(0.5, 1)
+}
+
+#' @export
+qq_theme <- function() theme_minimal()
+
+#' @export
+qq_plot <- function(pvals, title, ...)
+{
+  ### filter `pvals`
+  pvals <- pvals[!is.na(pvals)]
+  
+  num_pvals <- length(pvals)
+  
+  ### prepare `pvals`
+  pvals <- sort(pvals)
+  
+  pvals0 <- ppoints(length(pvals))
+  
+  ### plot
+  dat <- data_frame(val0 = -log10(pvals0), val = -log10(pvals))
+  p <- ggplot(dat, aes(val0, val)) + geom_point() 
+  
+  # line
+  p <- p + geom_abline(linetype = 3)
+  
+  # x/y limits
+  lims <- with(dat,
+    c(min(val0[num_pvals], val[num_pvals]), max(val0[1], val[1])))
+  
+  p <- p + xlim(lims) + ylim(lims)
+  
+  # labs
+  if(!missing(title)) {
+    p <- p + labs(title = title)
+  }
+  
+  p <- p + labs(x = "Expected -log(pval)", y = "Observed -log(pval)")
+  
+  # subtitle
+  lambda <- qq_inflation(pvals, ...)
+  
+  p <- p + labs(subtitle = paste0("inflation: ", round(lambda, 2)))
+  
+  
+  # theme
+  #p <- p + qq_theme()
+              
+  return(p)
+}
